97d48cf49385fec3719396755377e1e48bb1f9b7
Tayte Stokes <taytestokes9328@gmail.com>
2018-07-03 23:40:28 -0600

    Added Styling changes

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,6 +1,4 @@
-import React, {Component} from 'react'
-import {Link} from 'react-router-dom';
-
+import React, {Component} from 'react';
 
 //StyleSheets
 import 'reset-css';
@@ -9,45 +7,14 @@ import './Navbar.css';
 class Navbar extends Component{
   constructor(){
     super()
-
-    this.state = {
-      flag: true
-    }
   }
 
   render(){
     return (
       <div className="navbar-container">
-        <section className="navbar-main-design">
-          <ul>
-            <Link to="/"><li>Home</li></Link>
-            <Link to="/about"><li>About Me</li></Link>
-            <Link to=""><li>Portfolio</li></Link>
-          </ul>
-        </section>
-  
-        <section className="nav-menu-dropdown-container">
-          {
-            this.state.flag ? 
-            <div className="menu-patty-container" onClick={() =>    this.setState({flag: !this.state.flag})}>
-              <img src={require('../../assets/nav-icon.png')} alt="navbar-mobile-icon"/>
-            </div>
+        <div>
 
-            :
-            <div>
-              <div className="menu-patty-container" onClick={() => this.setState({flag: !this.state.flag})}>
-                <img src={require('../../assets/nav-icon.png')} alt="navbar-mobile-icon"/>
-              </div>
-
-              <div className="drop-down">
-                <section>About</section>
-                <section>Skills</section>
-                <section>Portfolio</section>
-                <section>Contact</section>
-              </div>
-            </div>
-          }
-        </section>
+        </div>
       </div>
     )
   }
